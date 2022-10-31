25d105c99783b52a5ec57815a58e8342b8a9990d
Vašek Chmel <vaclav.chmel@applifting.cz>
2022-10-31 14:13:17 +0100

    added firebase messaging to the project

---
+++ app/src/main/java/cz/applifting/graphqlempty/firebase/messaging/ChatMessagingService.kt
@@ -0,0 +1,10 @@
+package cz.applifting.graphqlempty.firebase.messaging
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class ChatMessagingService(): FirebaseMessagingService() {
+
+
+
+
+}
