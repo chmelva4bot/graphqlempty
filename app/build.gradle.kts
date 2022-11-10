dc3a775c6c2e10a1c8aefe08f1d5113fcbab0c15
Vašek Chmel <vaclav.chmel@applifting.cz>
2022-11-10 15:47:39 +0100

    toml dependencies done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    alias(libs.plugins.androidApplicationPlugin).apply(false)
-    alias(libs.plugins.kotlinAndroidPlugin).apply(false)
-    alias(libs.plugins.apollo3Plugin).apply(false)
-//    alias(libs.plugins.daggerHiltAndroidPlugin).apply(false)
-    alias(libs.plugins.googleServicesPlugin).apply(false)
+    alias(libs.plugins.androidApplicationPlugin)
+    alias(libs.plugins.kotlinAndroidPlugin)
+    alias(libs.plugins.apollo3Plugin)
+//    alias(libs.plugins.daggerHiltAndroidPlugin)
+    alias(libs.plugins.googleServicesPlugin)
 }
 
 android {
@@ -25,7 +25,7 @@ android {
 
     buildTypes {
         release {
-            minifyEnabled = false
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
@@ -34,7 +34,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = '1.8'
+        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
@@ -64,38 +64,22 @@ dependencies {
     implementation(libs.coilCompose)
     implementation(libs.androidxSecurity)
 
-    implementation("com.apollographql.apollo3:apollo-runtime:3.6.2")
+    implementation(libs.apollo3)
 
 //    implementation 'com.google.dagger:hilt-android:2.44'
 //    kapt 'com.google.dagger:hilt-compiler:2.44'
 //    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 //    kapt("androidx.hilt:hilt-compiler:1.0.0")
 
-    implementation "io.insert-koin:koin-androidx-compose:3.3.0"
+    implementation(libs.koinCompose)
 
-    // Google Sign In SDK
-    implementation 'com.google.android.gms:play-services-auth:20.2.0'
+    implementation(libs.bundles.firebase)
 
-    // Firebase SDK
-    implementation platform('com.google.firebase:firebase-bom:30.3.2')
-    implementation 'com.google.firebase:firebase-database-ktx'
-    implementation 'com.google.firebase:firebase-storage-ktx'
-    implementation 'com.google.firebase:firebase-auth-ktx'
-    implementation 'com.google.firebase:firebase-messaging-ktx'
 
-    // Firebase UI Library
-    implementation 'com.firebaseui:firebase-ui-auth:8.0.2'
-    implementation 'com.firebaseui:firebase-ui-database:8.0.2'
+    testImplementation(libs.bundles.test)
 
-    testImplementation 'junit:junit:4.13.2'
-    testImplementation "org.jetbrains.kotlinx:kotlinx-coroutines-test"
-    testImplementation("io.mockk:mockk:1.13.2")
-
-    androidTestImplementation 'androidx.test.ext:junit:1.1.3'
-    androidTestImplementation 'androidx.test.espresso:espresso-core:3.4.0'
-    androidTestImplementation "androidx.compose.ui:ui-test-junit4:$compose_ui_version"
-    debugImplementation "androidx.compose.ui:ui-tooling:$compose_ui_version"
-    debugImplementation "androidx.compose.ui:ui-test-manifest:$compose_ui_version"
+    androidTestImplementation(libs.bundles.androidTest)
+    debugImplementation(libs.bundles.debug)
 }
 
 kapt {
